19b70003a9afbdd35c54dbbf309039a33a774904
mahekjconnect <mahekjconnect@gmail.com>
2025-12-17 20:29:55 +0530

    17.12.15 update

---
+++ Python/Practical/17.12.2025.py
@@ -0,0 +1,103 @@
+#For loop
+
+colors = ["red","blue","green"]
+
+for color in colors:
+    print(color)
+print("---------------------")
+#range() function
+
+for x in range(1,13,2):
+    print(x)
+print("---------------------")
+#iteration over str
+
+for i in "Python":
+    print(i)
+print("---------------------")
+
+#nested for loop
+
+for i in range(1,4):
+    for j in range(1,4):
+        print(i,j)
+print("---------------------")
+
+#While loop
+
+i=0
+while i<=5:
+    print(i)
+    i=i+1
+print("---------------------")
+
+#break
+i=0
+while i<=5:
+    print(i)
+    if i==3:
+        break
+    i=i+1
+print("---------------------")
+
+#continue
+i=0
+while i<=5:
+    print(i)
+    if i==3:
+        i=i+1
+        continue
+    i=i+1
+print("---------------------")
+
+#pass
+
+for i in range(5):
+    pass
+
+print("---------------------")
+
+#DATA STRUCTURES
+
+# list 
+
+list = [1,2,3,4,5]
+print(list)
+"""for x in list:
+    print(x)"""
+
+list.append(29)
+list.insert(3,567)
+list.remove(5)
+list[1]=96
+len(list)
+
+for x in list:
+    print(x)
+print("---------------------")
+
+#Tuple
+
+tup=("mahek","sayma","zia")
+
+for x in tup:
+    print(x)
+print("---------------------")
+
+#set
+
+set1={1,2,3,4,5,5,6}
+
+print(set1)
+print("---------------------")
+
+#Dictionary
+
+dic = {"name" : "Mahek","surname": "Junnedi","age" : 19}
+print(dic)
+
+dic["age"]=18
+dic["course"] = "python"
+
+print(dic)
+print("---------------------")
